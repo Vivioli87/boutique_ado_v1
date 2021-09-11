2afd78693557d040b71761daa6f62e553e0ea208
Vivioli87 <vivnightingale@hotmail.com>
2021-09-11 17:14:13 +0000

    Added checkout form data caching in payment intent

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -10,9 +10,7 @@ import stripe
 @require_POST
 @csrf_exempt
 def webhook(request):
-    """
-    Listen for webhooks from Stripe
-    """
+    """Listen for webhooks from Stripe"""
     # Setup
     wh_secret = settings.STRIPE_WH_SECRET
     stripe.api_key = settings.STRIPE_SECRET_KEY
@@ -24,7 +22,7 @@ def webhook(request):
 
     try:
         event = stripe.Webhook.construct_event(
-            payload, sig_header, wh_secret
+        payload, sig_header, wh_secret
         )
     except ValueError as e:
         # Invalid payload
@@ -47,7 +45,7 @@ def webhook(request):
     # Get the webhook type from Stripe
     event_type = event['type']
 
-    # If theres a handler for it, get it from the event map
+    # If there's a handler for it, get it from the event map
     # Use the generic one by default
     event_handler = event_map.get(event_type, handler.handle_event)
 
